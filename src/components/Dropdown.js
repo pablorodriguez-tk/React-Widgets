6fe4af7d8cda48d9bdde35ad491c8a3f2d66699e
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-03-11 16:32:55 -0300

    Iniciado tercer widget - Dropdown

---
+++ src/components/Dropdown.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+const Dropdown = ({ options, selected, onSelectedChange }) => {
+  const [open, setOpen] = useState(false);
+
+  const renderedOptions = options.map((options) => {
+    if (options.value === selected.value) {
+      //El elemento seleccionado no aparecera en la lista
+      return null;
+    }
+    return (
+      <div
+        key={options.value}
+        className="item"
+        onClick={() => {
+          onSelectedChange(options);
+        }}
+      >
+        {options.label}
+      </div>
+    );
+  });
+
+  return (
+    <div className="ui form">
+      <div className="field">
+        <label className="label">Select a Color</label>
+        <div
+          onClick={() => setOpen(!open)}
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
+        >
+          <i className="dropdown icon"></i>
+          <div className="text">{selected.label}</div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>
+            {renderedOptions}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
